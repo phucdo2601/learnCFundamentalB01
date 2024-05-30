4fd7442548f992f5dc7e77f8a49ebdcfa0dfac23
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-30 10:09:08 +0700

    [learn-list-c-func-b01] 02. Basic learn escape sequence b01

--- 01-HelloWorld/main.c
+++ 01-HelloWorld/main.c
@@ -7,7 +7,11 @@ int main() {
      * This is a multiple lines comment
     */
 
-    printf("Hello World!");
+    printf("Hello\n World\n!\n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    printf("I like Pizza - \'Phucdn\'\n");
+    printf("I like Pizza - \\Steven Holwking\\");
 
     return 0;
 }
