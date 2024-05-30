6c54616d0a06329c4ca957e7dc5132a21add981b
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-30 13:41:29 +0700

    [learn-list-c-func-b01] 04. Basic learn data type in c b01

---
+++ 03-datatypes/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    // float a = 1.3;
+    // double b = 4.5;
+
+    // printf("Print float var: %f\n", a);
+    // printf("Print double var: %lf\n", b);
+
+    // bool d = true;
+    // printf("Print bool var: %d\n", d);
+
+    short  a = 32678;
+    unsigned short  i = 65535;
+
+    printf("Printf short int var: %d\n", a);
+    printf("Printf unsigned short int var: %d\n", i);
+
+}
