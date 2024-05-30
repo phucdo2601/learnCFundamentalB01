9b72ed36bade1a1eb69ffa0395c0568c98902435
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-30 20:52:58 +0700

    [learn-list-c-func-b01] 06. Basic learn user input in c b01

--- 05UuserInput/main.c
+++ 05UuserInput/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main()
+{
     char name[25];
     int age;
 
     printf("\nWhat 's your name?: ");
-    scanf("%s", &name);
+    // scanf("%s", &name);
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
 
     printf("\nHow old are you?: ");
     scanf("%d", &age);
+    // fgets(age, );
 
     printf("\nHello %s, how are you?\n", name);
     printf("\nYou are %d years old", age);
-
-    
-
 }
