1532886cbf41d5d464ffb7cb0c76ce505ff66194
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-30 20:54:15 +0700

    [learn-list-c-func-b01] 06. Basic learn user input in c b01

---
+++ les06-mathFunc/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A = sqrt(9);
+    double B = pow(2, 4);
+    double C = round(3.14);
+    double D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100);
+    double G = log(3);
+    double H = sin(45);
+    double I = tan(45);
+    double J = cos(45);
+
+    printf("A = %lf\n", A);
+
+    return 0;
+}
