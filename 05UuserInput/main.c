fc8fb773cd6b11728c33e39c9cc6627e67d01f2a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-30 14:38:43 +0700

    [learn-list-c-func-b01] 06. Basic learn user input in c b01

---
+++ 05UuserInput/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char name[25];
+    int age;
+
+    printf("\nWhat 's your name?: ");
+    scanf("%s", &name);
+
+    printf("\nHow old are you?: ");
+    scanf("%d", &age);
+
+    printf("\nHello %s, how are you?\n", name);
+    printf("\nYou are %d years old", age);
+
+    
+
+}
