42605f8f87cf56789741aff7c1bc439d5686a97a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-31 13:56:25 +0700

    [learn-list-c-func-b01] 12. Basic learn AND logical in c

---
+++ les12AndLog/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // logical operatos = && (And) checks if two conditions are true
+    float temp = 25;
+
+    if (temp > 10 && temp < 50)
+    {
+        printf("\nThe tempertuarer is avarage!");
+    } else {
+        printf("\nThe temperature is unavaiable!");
+    }
+    
+}
